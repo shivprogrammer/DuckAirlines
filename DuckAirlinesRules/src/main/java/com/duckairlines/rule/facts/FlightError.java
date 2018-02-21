01382c23c0f6129a98efa816bc592e4c23a4b9ac
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-21 04:57:44 -0800

    added FlightError class

---
+++ DuckAirlinesRules/src/main/java/com/duckairlines/rule/facts/FlightError.java
@@ -0,0 +1,13 @@
+package com.duckairlines.rule.facts;
+
+public class FlightError {
+	private String errorReason;
+
+	public String getErrorReason() {
+		return errorReason;
+	}
+
+	public void setErrorReason(String errorReason) {
+		this.errorReason = errorReason;
+	}
+}
