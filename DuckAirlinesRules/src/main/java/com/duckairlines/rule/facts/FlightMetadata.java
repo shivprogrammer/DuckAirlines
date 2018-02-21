7f404b89a55353d4a9fff287d6082a69001ea1a2
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-21 04:46:35 -0800

    adding private variables within FlightMetadata

--- DuckAirlinesRules/src/main/java/com/duckairlines/rule/facts/FlightMetadata.java
+++ DuckAirlinesRules/src/main/java/com/duckairlines/rule/facts/FlightMetadata.java
@@ -1,5 +1,12 @@
 package com.duckairlines.rule.facts;
 
 public class FlightMetadata {
-
+	private int aircraftRange;
+	private int distanceOfFlight;
+	private int aircraftTakeOfRoll;
+	private int aircraftLandingRollout;
+	private int originatingAirportRunwayLength;
+	private int destinationAirportRunwayLength;
+	private String aircraftType;
+	private String pilotRating;
 }
